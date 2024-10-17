c36f9c04127cae0928a525dc95f5d56ba3d4298b
nurbek0316 <maulethan.nm@gmail.com>
2024-10-17 21:17:39 +0500

    search krivoi poka chto

--- furniture-site/src/app/app.routes.ts
+++ furniture-site/src/app/app.routes.ts
@@ -1,9 +1,21 @@
+// import { Routes } from '@angular/router';
+// import { LoginComponent } from './login/login.component';
+// import { HomeComponent } from './home/home.component'; 
+
+// export const routes: Routes = [
+//   { path: 'login', component: LoginComponent },
+//   { path: '', component: HomeComponent, pathMatch: 'full' }, 
+//   { path: '**', redirectTo: '', pathMatch: 'full' } 
+// ];
 import { Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component'; 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent, pathMatch: 'full' }, 
-  { path: '**', redirectTo: '', pathMatch: 'full' } 
+  { path: 'search/:searchTerm', component: SearchComponent }, // <-- Add this route for search
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
+
--- furniture-site/src/app/search/search.component.ts
+++ furniture-site/src/app/search/search.component.ts
@@ -1,36 +1,82 @@
+// import { Component, OnInit } from '@angular/core';
+// import { RouterModule } from '@angular/router';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { FormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+
+
+
+
+// @Component({
+//   selector: 'app-search',
+//   templateUrl: './search.component.html',
+//   standalone: true,
+//   imports: [RouterModule, FormsModule ], 
+//   styleUrls: ['./search.component.css']
+// })
+// export class SearchComponent implements OnInit {
+
+//   searchTerm: string = ''; 
+
+//   constructor(private route: ActivatedRoute, private router: Router) { }
+
+//   ngOnInit(): void {
+//     this.route.params.subscribe(params => {
+//       if (params['searchTerm']) {
+//         this.searchTerm = params['searchTerm'];
+//       }
+//     });
+//   }
+
+//   search(): void {
+//     if (this.searchTerm) {
+//       this.router.navigateByUrl('/search/' + this.searchTerm);
+//     }
+//   }
+// }
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
-
-
+import { FurnService } from '../services/furn/furn.service'; // Ensure this import is correct
+import { Furn } from '../shared/Furn'; // Ensure this import is correct
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   standalone: true,
-  imports: [RouterModule, FormsModule ], 
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
+  imports: [FormsModule, CommonModule],
 })
 export class SearchComponent implements OnInit {
-
   searchTerm: string = ''; 
+  furns: Furn[] = []; // Array to hold search results
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute, 
+    private router: Router,
+    private furnService: FurnService // Inject the FurnService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       if (params['searchTerm']) {
         this.searchTerm = params['searchTerm'];
+        this.searchFurns(); // Call the search method when params change
       }
     });
   }
 
   search(): void {
     if (this.searchTerm) {
-      this.router.navigateByUrl('/search/' + this.searchTerm);
+      console.log(`Searching for: ${this.searchTerm}`);
+      this.router.navigate(['/search', this.searchTerm]); // Navigate to the search route
     }
   }
+
+  searchFurns(): void {
+    this.furns = this.furnService.getAllFurnBySearchTerm(this.searchTerm);
+    console.log(`Furns found: ${this.furns.length}`); // Log how many results are found
+  }
 }
+
